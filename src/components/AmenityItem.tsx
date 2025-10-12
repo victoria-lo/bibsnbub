9f62d263100c783a3d2dbd1d7605a26e418de047
Gitansha Aggarwal <43965406+gitansha@users.noreply.github.com>
2025-10-12 21:51:29 +0800

    feat: add diaper and linen dispensers to amenities list (#23)
    * feat: add diaper and linen dispensers to amenities list
    
    * fix: fixed the typo in amenities.json & changed to singular in AmenityItem.json

--- src/components/AmenityItem.tsx
+++ src/components/AmenityItem.tsx
@@ -1,6 +1,6 @@
 import type { Amenity } from '@/models/types';
 import { Badge } from '@/components/ui/badge';
-import { bottleBaby, faucet } from '@lucide/lab';
+import { bottleBaby, diaper, faucet, towelFolded } from '@lucide/lab';
 import BabyChangingStationIcon from '@mui/icons-material/BabyChangingStation';
 import { Coffee, Icon, Plug, Snowflake, Sofa, Trash2, Wifi, XCircle } from 'lucide-react';
 import React from 'react';
@@ -21,6 +21,10 @@ const AmenityItem: React.FC<AmenityItemProps> = ({ amenity, quantity }) => {
         return <Snowflake className="w-5 h-5 text-gray-500" />;
       case 'diaper changing station':
         return <BabyChangingStationIcon className="w-5 h-5 text-gray-600" />;
+      case 'diaper dispenser':
+        return <Icon className="w-5 h-5 text-gray-600" iconNode={diaper} />;
+      case 'bed linen dispenser':
+        return <Icon className="w-5 h-5 text-gray-600" iconNode={towelFolded} />;
       case 'wifi':
         return <Wifi className="w-5 h-5 text-gray-600" />;
       case 'trash bin':
